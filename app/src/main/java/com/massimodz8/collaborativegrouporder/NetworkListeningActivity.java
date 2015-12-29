058ef7ddff4e658ce42a4c72ab416defa23db3a9
Massimo Del Zotto <MassimoDZ8@gmail.com>
2015-12-29 16:21:26 +0100

    Successfully pulled up a network service.

--- app/src/main/java/com/massimodz8/collaborativegrouporder/NetworkListeningActivity.java
+++ app/src/main/java/com/massimodz8/collaborativegrouporder/NetworkListeningActivity.java
@@ -5,17 +5,19 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.IntentFilter;
-import android.net.wifi.p2p.WifiP2pDevice;
-import android.net.wifi.p2p.WifiP2pDeviceList;
-import android.net.wifi.p2p.WifiP2pManager;
-import android.net.wifi.p2p.WifiP2pManager.PeerListListener;
-import android.net.wifi.p2p.nsd.WifiP2pDnsSdServiceInfo;
+import android.net.nsd.NsdManager;
+import android.net.nsd.NsdServiceInfo;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
+import java.io.IOException;
+import java.net.ServerSocket;
 import java.util.Collection;
 import java.util.Iterator;
 
@@ -24,77 +26,32 @@ We open a Wi-Fi direct bubble and a proper service and listen to network to find
  */
 /** todo: this is an excellent moment to provide some ads: after the GM started scanning
  * he has to wait for users to join and I can push to him whatever I want.  */
-public class NetworkListeningActivity extends AppCompatActivity {
-    WifiP2pManager wifi;
-    WifiP2pManager.Channel peerNetwork;
+public class NetworkListeningActivity extends AppCompatActivity implements NsdManager.RegistrationListener {
+    private ServerSocket landing;
+    private NsdManager nsdService;
 
-    class WifiBroadcastReceiver extends BroadcastReceiver {
-        @Override
-        public void onReceive(final Context context, Intent intent) {
-            String action = intent.getAction();
-            if(WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION.equals(action)) {
-                int state = intent.getIntExtra(WifiP2pManager.EXTRA_WIFI_STATE, -1);
-                if(state != WifiP2pManager.WIFI_P2P_STATE_ENABLED) p2pFailed();
-            } else if (WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION.equals(action)) {
-                /*
-                This involves stuff such as
-                (WifiP2pDevice) intent.getParcelableExtra(WifiP2pManager.EXTRA_WIFI_P2P_DEVICE)
-                Which apparently provide some information about this device... I don't care about those things really...
-                 */
-                /// TODO: figure out what to do with WIFI_P2P_THIS_DEVICE_CHANGED_ACTION
-            } else if (WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION.equals(action)) {
-                /*
-                Again, I'm not sure what this really is, I'll have to look at it.
-                 */
-                /// TODO: figure out what to do with WIFI_P2P_CONNECTION_CHANGED_ACTION
-            } else if (WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION.equals(action)) {
-                wifi.requestPeers(peerNetwork, new PeerListListener() {
-                    @Override
-                    public void onPeersAvailable(WifiP2pDeviceList peers) {
-                        Collection<WifiP2pDevice> list = peers.getDeviceList();
-                        String shit = "";
-                        for(WifiP2pDevice el : list) {
-                            shit += el.toString() + '\n';
-                        }
-
-                        AlertDialog.Builder build = new AlertDialog.Builder(context);
-                        build.setTitle("Peers updated")
-                                .setMessage(shit);
-                        build.show();
-                        return;
-                    }
-                });
-             }
-        }
-    }
-
-
-    WifiBroadcastReceiver receiver;
-    IntentFilter intentFilter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_network_listening);
-
-        intentFilter = new IntentFilter();
-        intentFilter.addAction(WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION);
-        intentFilter.addAction(WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION);
-        intentFilter.addAction(WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION);
-        intentFilter.addAction(WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION);
+    }
+    protected void onDestroy() {
+        nsdService.unregisterService(this);
+        super.onDestroy();
     }
 
     @Override
     public void onResume() {
+        // regen service if I decide to tear it down.
         super.onResume();
-        receiver = new WifiBroadcastReceiver();
-        registerReceiver(receiver, intentFilter);
     }
 
     @Override
     public void onPause() {
+        // nsdService.unregisterService(this);
+        // I don't do this. I really want the service to stay on.
         super.onPause();
-        unregisterReceiver(receiver);
     }
 
     public void initiatePartyHandshake(View view) {
@@ -114,90 +71,75 @@ public class NetworkListeningActivity extends AppCompatActivity {
             return;
         }
 
-        wifi = (WifiP2pManager)getSystemService(Context.WIFI_P2P_SERVICE);
-        if(wifi == null) {
+        try {
+            landing = new ServerSocket(0);
+        } catch (IOException e) {
             AlertDialog.Builder build = new AlertDialog.Builder(this);
-            build.setTitle(R.string.nullWifi_title)
-                    .setMessage(R.string.nullWifi_msg);
+            build.setTitle(R.string.serverSocketFailed_title)
+                    .setMessage(R.string.serverSocketFailed_msg);
             build.show();
             return;
         }
-        receiver = new WifiBroadcastReceiver();
-        registerReceiver(receiver, intentFilter);
-        peerNetwork = wifi.initialize(this, getMainLooper(), null);
 
-        /*
-        The goal of this activity is to create the "group initiative" service but in P2P networking
-        the difference is subtle and peer discovery must take place anyway.
-        */
+        nsdService = (NsdManager)getSystemService(Context.NSD_SERVICE);
+        if(nsdService == null) {
+            AlertDialog.Builder build = new AlertDialog.Builder(this);
+            build.setTitle(R.string.nullNSDService_title)
+                    .setMessage(R.string.nullNSDService_msg);
+            build.show();
+        }
+
         final NetworkListeningActivity self = this;
-        wifi.discoverPeers(peerNetwork, new WifiP2pManager.ActionListener() {
+        serviceRegistrationHandler = new Handler(new Handler.Callback() {
             @Override
-            public void onSuccess() { /* assume it'll work */ }
+            public boolean handleMessage(Message msg) {
+                switch(msg.what) {
+                    case MSG_SERVICE_REGISTRATION_COMPLETE:
+                        ServiceRegistrationResult res = (ServiceRegistrationResult)msg.obj;
+                        if(res == null) res = new ServiceRegistrationResult(-1); // impossible by construction... for now.
+                        if(res.successful) {
+                            if(!res.netName.equals(gname)) {
+                                TextView note = (TextView)findViewById(R.id.txt_renamedService);
+                                String text = getString(R.string.renamedGroup);
+                                note.setText(String.format(text, res.netName));
+                                note.setVisibility(View.VISIBLE);
+                            }
 
-            @Override
-            public void onFailure(int reasonCode) { self.peerDiscoveryFailed(reasonCode); }
+                            findViewById(R.id.txt_scanning).setVisibility(View.VISIBLE);
+                            findViewById(R.id.progressBar).setVisibility(View.VISIBLE);
+                            findViewById(R.id.list_characters).setVisibility(View.VISIBLE);
+                            View closeGroupBtn = findViewById(R.id.btn_closeGroup);
+                            closeGroupBtn.setVisibility(View.VISIBLE);
+                            closeGroupBtn.setEnabled(false);
+                        }
+                        else {
+                            AlertDialog.Builder build = new AlertDialog.Builder(self);
+                            String readable;
+                            switch(res.error) {
+                                case NsdManager.FAILURE_ALREADY_ACTIVE: readable = "FAILURE_ALREADY_ACTIVE"; break;
+                                case NsdManager.FAILURE_INTERNAL_ERROR: readable = "FAILURE_INTERNAL_ERROR"; break;
+                                case NsdManager.FAILURE_MAX_LIMIT: readable = "FAILURE_MAX_LIMIT"; break;
+                                default: readable = String.format("%1$d", res.error);
+                            }
+                            build.setTitle(R.string.serviceRegFailed_title)
+                                    .setMessage(String.format(getString(R.string.serviceRegFailed_msg), readable));
+                            build.show();
+                        }
+                        return true;
+                }
+                return false;
+            }
         });
-
-        WifiP2pDnsSdServiceInfo servDesc = WifiP2pDnsSdServiceInfo.newInstance(gname, "_groupInitiative._tcp", null);
-        wifi.addLocalService(peerNetwork, servDesc, new WifiP2pManager.ActionListener() {
-            @Override
-            public void onSuccess() { /* assume completed. */ }
-
-            @Override
-            public void onFailure(int reasonCode) { self.serviceInstallFailed(reasonCode); }
-        });
-
-        /*
-        wifi.discoverPeers(peerNetwork, new WifiP2pManager.ActionListener() {
-        @Override
-        public void onSuccess() { /* assume discover init is ok * / }
-
-        @Override
-        public void onFailure(int reasonCode) { self.discoveryFailed(reasonCode); }
-        });
-        */
-
-
-        /*
-         more peer network work. I will have to think at this carefully.
-         First find the peers, then handshake them, show them on the list... but the list
-         is about characters!
-        */
-
-
+        NsdServiceInfo servInfo  = new NsdServiceInfo();
+        servInfo.setServiceName(gname);
+        servInfo.setServiceType("_groupInitiative._tcp");
+        servInfo.setPort(landing.getLocalPort());
+        nsdService.registerService(servInfo, NsdManager.PROTOCOL_DNS_SD, this);
 
         view.setEnabled(false);
         groupNameView.setEnabled(false);
-        findViewById(R.id.txt_scanning).setVisibility(View.VISIBLE);
-        findViewById(R.id.progressBar).setVisibility(View.VISIBLE);
-        findViewById(R.id.list_characters).setVisibility(View.VISIBLE);
-        View closeGroupBtn = findViewById(R.id.btn_closeGroup);
-        closeGroupBtn.setVisibility(View.VISIBLE);
-        closeGroupBtn.setEnabled(false);
-
-
-        // Initiate WiFi-direct handshake and services.
-
-        // Eventually show service name if colliding!
-
     }
 
-    private void serviceInstallFailed(int reason) {
-        disableButtons();
-
-        String readable = "<Unknown error>";
-        switch(reason) {
-            case WifiP2pManager.P2P_UNSUPPORTED: readable = "P2P_UNSUPPORTED"; break;
-            case WifiP2pManager.ERROR: readable = "ERROR"; break;
-            case WifiP2pManager.BUSY: readable = "BUSY"; break;
-        }
-
-        AlertDialog.Builder build = new AlertDialog.Builder(this);
-        build.setTitle(R.string.serviceInstallFailed_title)
-                .setMessage(String.format(getString(R.string.serviceInstallFailed_msg), readable));
-        build.show();
-    }
 
     private void disableButtons() {
         findViewById(R.id.btn_closeGroup).setEnabled(false);
@@ -206,28 +148,44 @@ public class NetworkListeningActivity extends AppCompatActivity {
         findViewById(R.id.list_characters).setEnabled(false);
     }
 
-
-    private void peerDiscoveryFailed(int reason) {
-        disableButtons();
-
-        String readable = "<Unknown error>";
-        switch(reason) {
-            case WifiP2pManager.P2P_UNSUPPORTED: readable = "P2P_UNSUPPORTED"; break;
-            case WifiP2pManager.ERROR: readable = "ERROR"; break;
-            case WifiP2pManager.BUSY: readable = "BUSY"; break;
+    // NsdManager.RegistrationListener() is async AND on a different thread so I cannot just
+    // modify the various controls from there. Instead, wait for success/fail and then
+    // pass a notification to the UI thread.
+    public static final int MSG_SERVICE_REGISTRATION_COMPLETE = 1;
+    class ServiceRegistrationResult {
+        public ServiceRegistrationResult(String netName) {
+            this.netName = netName;
+            successful = true;
+        }
+        public ServiceRegistrationResult(int err) {
+            error = err;
+            successful = false;
         }
 
-        AlertDialog.Builder build = new AlertDialog.Builder(this);
-        build.setTitle(R.string.discoveryFailed_title)
-                .setMessage(String.format(getString(R.string.discoveryFailed_msg), readable));
-        build.show();
+        public int error;
+        public boolean successful;
+        public String netName;
     }
-    private void p2pFailed() {
-        disableButtons();
+    Handler serviceRegistrationHandler;
 
-        AlertDialog.Builder build = new AlertDialog.Builder(this);
-        build.setTitle(R.string.unexpectedP2PStateChange_title)
-                .setMessage(getString(R.string.unexpectedP2PStateChange_msg));
-        build.show();
+    //
+    // NsdManager.RegistrationListener() ___________________________________________________________
+    @Override
+    public void onServiceRegistered(NsdServiceInfo info) {
+        final String netName = info.getServiceName();
+        Message msg = Message.obtain(serviceRegistrationHandler, MSG_SERVICE_REGISTRATION_COMPLETE, new ServiceRegistrationResult(netName));
+        serviceRegistrationHandler.sendMessage(msg);
     }
+
+    @Override
+    public void onRegistrationFailed(NsdServiceInfo serviceInfo, int errorCode) {
+        Message msg = Message.obtain(serviceRegistrationHandler, MSG_SERVICE_REGISTRATION_COMPLETE, new ServiceRegistrationResult(errorCode));
+        serviceRegistrationHandler.sendMessage(msg);
+    }
+
+    @Override
+    public void onServiceUnregistered(NsdServiceInfo arg0) { }
+
+    @Override
+    public void onUnregistrationFailed(NsdServiceInfo serviceInfo, int errorCode) { }
 }
