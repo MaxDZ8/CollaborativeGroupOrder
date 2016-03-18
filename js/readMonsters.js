457291bbf5a78309ebd326eb6c56be02fb7809bd
Massimo Del Zotto <MassimoDZ8@gmail.com>
2016-03-18 16:04:54 +0100

    Parse first two monsters.
    --HG--
    branch : tool-monsterShrink

--- js/readMonsters.js
+++ js/readMonsters.js
@@ -13,9 +13,11 @@ window.onload = function() {
 		reader.onload = function() {
 			monsters = parseMonsterList(friendlify(reader.result));
 			document.getElementById('listInput').disabled = true;
-			var inner = "";
-			for(var loop = 0; loop < monsters.length; loop++) inner += "<tr><td>" + (loop + 1) + "</td><td>" + monsters[loop].engName + "</td></tr>";
-			parseListFeedback.innerHTML = inner;
+			for(var loop = 0; loop < monsters.length; loop++) {
+				monsters[loop].feedbackRow = document.createElement("TR");
+				monsters[loop].feedbackRow.innerHTML = "<td>" + (loop + 1) + "</td><td>" + monsters[loop].engName + "</td>";
+				parseListFeedback.appendChild(monsters[loop].feedbackRow);
+			}
 			document.body.appendChild(parseListFeedback);
 			document.body.appendChild(realDeal);
 			document.getElementById('realDealInput').onchange = loadFullText;
@@ -66,9 +68,9 @@ function parseBestiary(monsters, book) {
 	Match index name, then fire up the regexps. If the regexp'd blocks are contiguous (only separated by whitespace) then
 	we have a global match. */
 	//                             Sometimes, an example such as "Aasimar cleric 1"
-	//         CR integer or fraction|      |XPs|       |     |               alignment                   | Size| |Type      |Initiative     |
-	//                 |     \1      |      | \3|       v     |                  \4                       | |\5 | |\6|       |\7             |
-	var header = /\tCR (\d+(?:\/\d+)?)\n+XP (\d+)\n+(?:.+\n+)?(CE\s|CN\s|CG\s|NE\s|N\s|NG\s|LE\s|LN\s|LG\s)(\w+) (.+)\n+Init ([+\-]?\d+);.*\n+/;
+	//         CR integer or fraction|       |          XPs:    3,400               |       |     |               alignment                   | Size| |Type      |Initiative     |
+	//                 |     \1      |       |                \3                    |       v     |                  \4                       | |\5 | |\6|       |\7             |
+	var header = /\s+CR (\d+(?:\/\d+)?)\n+XP ((?:(?:\d?\d?\d,){1,3}\d\d\d)|\d?\d?\d?)\n+(?:.+\n+)?(CE\s|CN\s|CG\s|NE\s|N\s|NG\s|LE\s|LN\s|LG\s)(\w+) (.+)\n+Init ([+\-]?\d+);.*\n+/;
 	//                                         |AC |        dice count, type and mod           |          | TS cos        |      | TS dex        |       | TS wis        |
 	//                                         |\1 |              |\2                          |          |\3             |      |\4             |       |               |
 	var defense = /\n+(?:Defense|DEFENSE)\n+AC (\d+),.+\n+hp \d+ \((\d+d\d+(?:(?:\+|-)(?:\d+))?)\)\n+Fort ((?:\+|-)(?:\d+)), Ref ((?:\+|-)(?:\d+)), Will ((?:\+|-)(?:\d+))\n+/;
@@ -87,6 +89,28 @@ function parseBestiary(monsters, book) {
 	    	for(var loop = 0; loop < monsters.length; loop++) { // simple 'regular' monsters
 	    		if(monsters[loop].mangled) continue;
 	    		monsters[loop].mangled = matchMonster(monsters[loop].engName, monsters[loop + 1]);
+				if(monsters[loop].mangled) {
+					var data = monsters[loop].mangled;
+					var string = cell('Regular'); // parse type
+					string += cell(data.head[1]); // Challange Ratio
+					string += cell(data.head[2]); // XP
+					string += cell(data.head[3]); // alignment
+					string += cell(data.head[4]); // size
+					// string += cell(data.head[5]); // "type" example: outsider (native)
+					string += cell(data.head[6]); // initiative
+					string += cell(data.def[1]); // AC
+					string += cell(data.def[2]); // dice count and bonus
+					string += cell('F' + data.def[3] + ' R' + data.def[4] + ' W' + data.def[5]); // save
+					string += cell(data.off[1]); // speed
+					// string += cell(data.off[2]); // flying, in armor, swimming...
+					string += cell(data.stats[1]); // Str
+					string += cell(data.stats[2]); // Dex
+					string += cell(data.stats[3]); // Cos
+					string += cell(data.stats[4]); // Int
+					string += cell(data.stats[5]); // Wis
+					string += cell(data.stats[6]); // Cha
+					monsters[loop].feedbackRow.innerHTML += string;
+				}
 	    	}
 	    	lenDiff = book.length - prevLen;
 	    }
@@ -104,12 +128,16 @@ function parseBestiary(monsters, book) {
 			skipDiff = where - skip + name.length;
 			var imbad = book.substr(where);
 			var head = imbad.match(header);
-			if(!head) {
+			if(!head) continue;
+			var headPos = book.indexOf(head[0], skip);
+			if(fixSpaces(name, lineStart(headPos))) {
+				skip--;
+				skipDiff = 1;
 				continue;
 			}
 			// It's really a match if we reached header by getting no newlines and only whitespace,
 			// since the header includes the initial \t, they must simply be contiguous.
-			if(where + name.length !== book.indexOf(head[0], skip)) {
+			if(where + name.length !== headPos) {
 				//skipDiff += head[0].length; // don't skip head here, there are sometimes spurious characters.
 				continue;
 			}
@@ -139,4 +167,39 @@ function parseBestiary(monsters, book) {
 		parsed = parsed.replace(/\n\n/g, "\n").replace(/./g, ""); // "\u2588" <- blacken out
 		book = prev + parsed + then;
 	}
+	
+	// Given a position in the book, go to the character immediately following the previous newline.
+	// Identity if book[position] is newline or pos === 0.
+	function lineStart(pos) {
+		while(pos > 0 && book.charAt(pos) !== '\n') pos--;
+		if(pos === 0) return pos;
+		return pos + 1;
+	}
+	
+	// Sometimes copypasting makes stupid shit, such as adding spaces to our strings... we cannot allow that so...
+	function fixSpaces(name, index) {
+		if(index === book.indexOf(name, index)) return false;
+		var begin = index;
+		var tokens = name.split(/\s+/g);
+		for(var loop = 0; loop < tokens.length; loop++) {
+			var el = tokens[loop];
+			if(book.substr(index, el.length) === el) index += el.length;
+			else { // try removing a space
+			    var got = book.substr(index, el.length + 1).replace(" ", "");
+				if(got === el) index += got.length + 1;
+				else return false;
+			}
+			if(loop + 1 < tokens.length) while(index < book.length && book.charAt(index) === ' ') index++;
+		}
+		// If here we found a match. Replace the string with what I need.
+		var conditioned = book.substr(0, begin);
+		conditioned += name;
+		conditioned += book.substr(index);
+		book = conditioned;
+		return true;
+	}
+}
+
+function cell(string) {
+	return '<td>' + string + '</td>';
 }
