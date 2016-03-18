b6e33e02ec1404a1975a44713fcda727110ac242
Massimo Del Zotto <MassimoDZ8@gmail.com>
2016-03-18 16:51:46 +0100

    tab -> spaces again.
    --HG--
    branch : tool-monsterShrink

--- js/readMonsters.js
+++ js/readMonsters.js
@@ -1,101 +1,101 @@
 "use strict";
 
 window.onload = function() {
-	var parseListFeedback = document.getElementById('parseListFeedback');
-	var realDeal = document.getElementById('realDeal');
-	var realDealFeedback = document.getElementById('realDealFeedback');
-	parseListFeedback.parentNode.removeChild(parseListFeedback);
-	realDeal.parentNode.removeChild(realDeal);
-	realDealFeedback.parentNode.removeChild(realDealFeedback);
-	var monsters = [];
-	document.getElementById('listInput').onchange = function(ev) {
-		var reader = new FileReader();
-		reader.onload = function() {
-			monsters = parseMonsterList(friendlify(reader.result));
-			document.getElementById('listInput').disabled = true;
-			for(var loop = 0; loop < monsters.length; loop++) {
-				monsters[loop].feedbackRow = document.createElement("TR");
-				monsters[loop].feedbackRow.innerHTML = "<td>" + (loop + 1) + "</td><td>" + monsters[loop].engName + "</td>";
-				parseListFeedback.appendChild(monsters[loop].feedbackRow);
-			}
-			document.body.appendChild(parseListFeedback);
-			document.body.appendChild(realDeal);
-			document.getElementById('realDealInput').onchange = loadFullText;
-		};
-		reader.readAsText(document.getElementById('listInput').files[0]);
-	};
-	
-	function loadFullText(ev) {
-		var reader = new FileReader();
-		reader.onload = function() {
-			parseBestiary(monsters, friendlify(reader.result));
-		};
-		reader.readAsText(document.getElementById('realDealInput').files[0]);
-	}
+    var parseListFeedback = document.getElementById('parseListFeedback');
+    var realDeal = document.getElementById('realDeal');
+    var realDealFeedback = document.getElementById('realDealFeedback');
+    parseListFeedback.parentNode.removeChild(parseListFeedback);
+    realDeal.parentNode.removeChild(realDeal);
+    realDealFeedback.parentNode.removeChild(realDealFeedback);
+    var monsters = [];
+    document.getElementById('listInput').onchange = function(ev) {
+        var reader = new FileReader();
+        reader.onload = function() {
+            monsters = parseMonsterList(friendlify(reader.result));
+            document.getElementById('listInput').disabled = true;
+            for(var loop = 0; loop < monsters.length; loop++) {
+                monsters[loop].feedbackRow = document.createElement("TR");
+                monsters[loop].feedbackRow.innerHTML = "<td>" + (loop + 1) + "</td><td>" + monsters[loop].engName + "</td>";
+                parseListFeedback.appendChild(monsters[loop].feedbackRow);
+            }
+            document.body.appendChild(parseListFeedback);
+            document.body.appendChild(realDeal);
+            document.getElementById('realDealInput').onchange = loadFullText;
+        };
+        reader.readAsText(document.getElementById('listInput').files[0]);
+    };
+    
+    function loadFullText(ev) {
+        var reader = new FileReader();
+        reader.onload = function() {
+            parseBestiary(monsters, friendlify(reader.result));
+        };
+        reader.readAsText(document.getElementById('realDealInput').files[0]);
+    }
 }
 
 
 function friendlify(string) {
-	return string.replace(/\u2013|\u2014/g, "-").replace(/\r/g, "\n");
+    return string.replace(/\u2013|\u2014/g, "-").replace(/\r/g, "\n");
 }
 
 
 function parseMonsterList(mobs) {
-	var list = mobs.split(/\s+\d+(?:-\d+)?\n\n/m);
-	if(list[list.length - 1] === "") list.length--;
-	var out = [];
-	var subType = /\(.*\)/;
-	var parAway = /\(|\)/g;
-	for(var loop = 0; loop < list.length; loop++) {
-		var el = list[loop];
-		var par = el.match(subType);
-		var build = {};
-		if(par) el = el.replace(par, "").trim();
-		build.engName = el;
-		if(par) build.subType = par[0];
-		if(build.subType) build.subType = build.subType.replace(parAway, "");
-		out.push(build);
-	}
-	return out;
+    var list = mobs.split(/\s+\d+(?:-\d+)?\n\n/m);
+    if(list[list.length - 1] === "") list.length--;
+    var out = [];
+    var subType = /\(.*\)/;
+    var parAway = /\(|\)/g;
+    for(var loop = 0; loop < list.length; loop++) {
+        var el = list[loop];
+        var par = el.match(subType);
+        var build = {};
+        if(par) el = el.replace(par, "").trim();
+        build.engName = el;
+        if(par) build.subType = par[0];
+        if(build.subType) build.subType = build.subType.replace(parAway, "");
+        out.push(build);
+    }
+    return out;
 }
 
 function parseBestiary(monsters, book) {
-	/* TODO: add "qualifiers" there! They can be for example:
-	- Giant - not a size!
-	- Young, adult - see dragons
-	- ?
-	We parse increasingly more difficult stuff.
-	Match index name, then fire up the regexps. If the regexp'd blocks are contiguous (only separated by whitespace) then
-	we have a global match. */
-	//                             Sometimes, an example such as "Aasimar cleric 1"
-	//         CR integer or fraction|       |          XPs:    3,400               |       |     |               alignment                   | Size| |Type      |Initiative     |
-	//                 |     \1      |       |                \3                    |       v     |                  \4                       | |\5 | |\6|       |\7             |
-	var header = /\s+CR (\d+(?:\/\d+)?)\n+XP ((?:(?:\d?\d?\d,){1,3}\d\d\d)|\d?\d?\d?)\n+(?:.+\n+)?(CE\s|CN\s|CG\s|NE\s|N\s|NG\s|LE\s|LN\s|LG\s)(\w+) (.+)\n+Init ([+\-]?\d+);.*\n+/;
-	//                                         |AC |        dice count, type and mod           |          | TS cos        |      | TS dex        |       | TS wis        |
-	//                                         |\1 |              |\2                          |          |\3             |      |\4             |       |               |
-	var defense = /\n+(?:Defense|DEFENSE)\n+AC (\d+),.+\n+hp \d+ \((\d+d\d+(?:(?:\+|-)(?:\d+))?)\)\n+Fort ((?:\+|-)(?:\d+)), Ref ((?:\+|-)(?:\d+)), Will ((?:\+|-)(?:\d+))\n+/;
-	//                                             speed   || extra speed modifiers such as fly, swim 
-	//                                            |\1      ||\2|
-	var offense = /\n+(?:Offense|OFFENSE)\n+Speed (\d+ ft\.)(.*)\n+/; // this one is very complicated!
-	//
-	//
-	var statistics = /\n+(?:Statistics|STATISTICS)\n+Str (\d+|-), Dex (\d+|-), Con (\d+|-), Int (\d+|-), Wis (\d+|-), Cha (\d+|-)\n+/;
-	transmogrify();
-	
-	function transmogrify() {
-	    var lenDiff = -1;
-	    while(lenDiff !== 0) {
-	    	var prevLen = book.length;
-	    	for(var loop = 0; loop < monsters.length; loop++) { // simple 'regular' monsters
-	    		if(monsters[loop].mangled) continue;
-	    		monsters[loop].mangled = matchMonster(monsters[loop].engName, monsters[loop + 1]);
-				if(monsters[loop].mangled) {
-					var data = monsters[loop].mangled;
-					var string = cell('Regular'); // parse type
-					string += cell(data.head[1]); // Challange Ratio
-					string += cell(data.head[2]); // XP
-					string += cell(data.head[3]); // alignment
-			        string += cell(data.head[4]); // size
+    /* TODO: add "qualifiers" there! They can be for example:
+    - Giant - not a size!
+    - Young, adult - see dragons
+    - ?
+    We parse increasingly more difficult stuff.
+    Match index name, then fire up the regexps. If the regexp'd blocks are contiguous (only separated by whitespace) then
+    we have a global match. */
+    //                             Sometimes, an example such as "Aasimar cleric 1"
+    //         CR integer or fraction|       |          XPs:    3,400               |       |     |               alignment                   | Size| |Type      |Initiative     |
+    //                 |     \1      |       |                \3                    |       v     |                  \4                       | |\5 | |\6|       |\7             |
+    var header = /\s+CR (\d+(?:\/\d+)?)\n+XP ((?:(?:\d?\d?\d,){1,3}\d\d\d)|\d?\d?\d?)\n+(?:.+\n+)?(CE\s|CN\s|CG\s|NE\s|N\s|NG\s|LE\s|LN\s|LG\s)(\w+) (.+)\n+Init ([+\-]?\d+);.*\n+/;
+    //                                         |AC |        dice count, type and mod           |          | TS cos        |      | TS dex        |       | TS wis        |
+    //                                         |\1 |              |\2                          |          |\3             |      |\4             |       |               |
+    var defense = /\n+(?:Defense|DEFENSE)\n+AC (\d+),.+\n+hp \d+ \((\d+d\d+(?:(?:\+|-)(?:\d+))?)\)\n+Fort ((?:\+|-)(?:\d+)), Ref ((?:\+|-)(?:\d+)), Will ((?:\+|-)(?:\d+))\n+/;
+    //                                             speed   || extra speed modifiers such as fly, swim 
+    //                                            |\1      ||\2|
+    var offense = /\n+(?:Offense|OFFENSE)\n+Speed (\d+ ft\.)(.*)\n+/; // this one is very complicated!
+    //
+    //
+    var statistics = /\n+(?:Statistics|STATISTICS)\n+Str (\d+|-),\s+Dex (\d+|-),\s+Con (\d+|-),\s+Int (\d+|-),\s+Wis (\d+|-),\s+Cha (\d+|-)\n+/;
+    transmogrify();
+    
+    function transmogrify() {
+        var lenDiff = -1;
+        while(lenDiff !== 0) {
+            var prevLen = book.length;
+            for(var loop = 0; loop < monsters.length; loop++) { // simple 'regular' monsters
+                if(monsters[loop].mangled) continue;
+                monsters[loop].mangled = matchMonster(monsters[loop].engName, monsters[loop + 1]);
+                if(monsters[loop].mangled) {
+                    var data = monsters[loop].mangled;
+                    var string = cell('Regular'); // parse type
+                    string += cell(data.head[1]); // Challange Ratio
+                    string += cell(data.head[2]); // XP
+                    string += cell(data.head[3]); // alignment
+                    string += cell(data.head[4]); // size
                     // string += cell(data.head[5]); // "type" example: outsider (native)
                     string += cell(data.head[6]); // initiative
                     string += cell(data.def[1]); // AC
@@ -162,9 +162,8 @@ function parseBestiary(monsters, book) {
     
     function blackenParsed(start, limit) {
         var prev = book.substring(0, start); // substring index, index. substr index, count
-        var parsed = book.substring(start, limit);
+        var parsed = "\n\n\n\n\n\n\n"; // trying to keep the line count is honorable but not really useful at all.
         var then = book.substring(limit);
-        parsed = parsed.replace(/\n\n/g, "\n").replace(/./g, ""); // "\u2588" <- blacken out
         book = prev + parsed + then;
     }
     
