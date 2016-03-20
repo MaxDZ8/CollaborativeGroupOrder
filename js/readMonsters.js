b83606374cfa37c0b04e00bdea439c2d6cf45482
Massimo Del Zotto <MassimoDZ8@gmail.com>
2016-03-20 13:07:47 +0100

    Fully parsed 'defense' but perhaps  I should use less regexp and more stateful..
    --HG--
    branch : tool-monsterShrink

--- js/readMonsters.js
+++ js/readMonsters.js
@@ -77,8 +77,8 @@ function partitions(book) {
     // So, what I do is: I extract all the various headers and everything to the starting newline, which should be monster's name.
     //                                                              Sometimes, an example such as "Aasimar cleric 1"
     //                                                                                      |
-    //          CR integer or fraction|      |          XPs:    3,400               |       |     |                              alignment                                      |Size| |Type                             |Initiative     |
-    //                  |     \1      |      |                \3                    |       v     |                                 \4                                          ||\5 | |\6                      |        |\7             |
+    //          CR integer or fraction|      |          XPs:    3,400               |       |     |                              alignment                                        |Size| |Type                        |Initiative 
+    //                  |     \1      |      |                \3                    |       v     |                                 \4                                            ||\5 | |\6                 |        |\7         
     let header = /\s+CR (\d+(?:\/\d+)?)\n+XP ((?:(?:\d?\d?\d,){1,3}\d\d\d)|\d?\d?\d?)\n+(?:.+\n+)?(CE\s|CN\s|CG\s|NE\s|N\s|NG\s|LE\s|LN\s|LG\s|Any alignment \(same as creator\)\s)(\w+) (.+(?:\s+\([^)]+\))?)\n+Init ([+\-]?\d+);.*\n+/;
     let cand = [];
     let head = book.match(header);
@@ -146,27 +146,7 @@ function parseMonsterList(mobs) {
 
 
 function parseMonster(interval) {
-	if(!interval || !interval.body) return;
-	let mangle = interval.body;
-	// With an header and an interval already parsed all we have to do is to the other stuff and comes super easy.
-    //                                         |AC |        dice count, type and mod           |          | TS cos        |      | TS dex        |       | TS wis        |
-    //                                         |\1 |              |\2                          |          |\3             |      |\4             |       |               |
-    const defense = /\n+(?:Defense|DEFENSE)\n+AC (\d+),.+\n+hp \d+ \((\d+d\d+(?:(?:\+|-)(?:\d+))?)\)\n+Fort ((?:\+|-)(?:\d+)), Ref ((?:\+|-)(?:\d+)), Will ((?:\+|-)(?:\d+))\n+/;
-    //                                             speed   || extra speed modifiers such as fly, swim 
-    //                                            |\1      ||\2|
-    const offense = /\n+(?:Offense|OFFENSE)\n+Speed (\d+ ft\.)(.*)\n+/; // this one is very complicated!
-    //
-    //
-    const statistics = /\n+(?:Statistics|STATISTICS)\n+Str (\d+|-),\s+Dex (\d+|-),\s+Con (\d+|-),\s+Int (\d+|-),\s+Wis (\d+|-),\s+Cha (\d+|-)\n+/;
-	
-	const def = mangle.match(defense);
-	if(!def) return;
-	mangle = '\n' + mangle.substr(def.index + def[0].length);
-	const off = mangle.match(offense);
-	if(!off) return;
-    mangle = '\n' + mangle.substr(off.index + off[0].length);
-    const stats = mangle.match(statistics);
-    if(!stats) return;
+	if(!interval || !interval.body || !interval.header) return;
     let parsed = "";
     parsed = cell('Regular'); // parse type
     parsed += cell(interval.header[1]); // Challange Ratio
@@ -175,18 +155,88 @@ function parseMonster(interval) {
     parsed += cell(interval.header[4]); // size
     // parsed += cell(interval.header[5]); // "type" example: outsider (native)
     parsed += cell(interval.header[6]); // initiative
-    parsed += cell(def[1]); // AC
-    parsed += cell(def[2]); // dice count and bonus
-    parsed += cell('F' + def[3] + ' R' + def[4] + ' W' + def[5]); // save
-    parsed += cell(off[1]); // speed
-    // parsed += cell(off[2]); // flying, in armor, swimming...
-    parsed += cell(stats[1]); // Str
-    parsed += cell(stats[2]); // Dex
-    parsed += cell(stats[3]); // Cos
-    parsed += cell(stats[4]); // Int
-    parsed += cell(stats[5]); // Wis
-    parsed += cell(stats[6]); // Cha
     interval.feedbackRow.innerHTML += parsed;
+	
+	let mangle = interval.body;
+	// With an header and an interval already parsed all we have to do is to the other stuff and comes super easy.
+    //                                             speed   || extra speed modifiers such as fly, swim 
+    //                                            |\1      ||\2|
+    const offense = /\n+(?:Offense|OFFENSE)\n+Speed (\d+ ft\.)(.*)\n+/; // this one is very complicated!
+    //
+    //
+    const statistics = /\n+(?:Statistics|STATISTICS)\n+Str (\d+|-),\s+Dex (\d+|-),\s+Con (\d+|-),\s+Int (\d+|-),\s+Wis (\d+|-),\s+Cha (\d+|-)\n+/;
+	
+	const def = parseDefense();
+	if(!def) return;
+	mangle = '\n' + mangle.substring(def.start, def.end);
+	////const off = mangle.match(offense);
+	////if(!off) return;
+    ////mangle = '\n' + mangle.substr(off.index + off[0].length);
+    ////const stats = mangle.match(statistics);
+    ////if(!stats) return;
+	parsed = "";
+    parsed += cell(def.ac); // AC
+    parsed += cell(def.health + brApp(def.hdCount) + brApp(def.healthNotes)); // dice count and bonus
+    parsed += cell('F' + def.fort + ' R' + def.refl + ' W' + def.will); // save
+    ////parsed += cell(off[1]); // speed
+    ////// parsed += cell(off[2]); // flying, in armor, swimming...
+    ////parsed += cell(stats[1]); // Str
+    ////parsed += cell(stats[2]); // Dex
+    ////parsed += cell(stats[3]); // Cos
+    ////parsed += cell(stats[4]); // Int
+    ////parsed += cell(stats[5]); // Wis
+    ////parsed += cell(stats[6]); // Cha
+    interval.feedbackRow.innerHTML += parsed;
+	
+	function signed(num) {
+		if(num > 0) return '+' + num;
+		return '' + num;
+	}
+	
+	function brApp(str) {
+		if(!str) return "";
+		return str + '<br>';
+	}
+	
+	// Stateful parsing >> RegExp
+	function parseDefense() {
+		const start = mangle.match(/\n(?:Defense|DEFENSE)\n+AC\s(\d+),\s/);
+		if(!start) return;
+		let index = start.index + start[0].length;
+		index = nextLineWithPar(mangle, index);
+		if(index === mangle.length) return;
+		let sub = mangle.substr(index);
+		let hp = sub.match(/^hp \d+ \((\d+\sHD;\s)?(\d+d\d+(?:\+\d+d\d+)?(?:[+-]\d+)?)([A-Za-z 0-9]*)\)/);
+		if(!hp) return;
+		sub = sub.substr(hp.index);
+		index = nextLineWithPar(mangle, index);
+		if(index === mangle.length) return;
+		let save = sub.match(/Fort ((?:\+|-)\d+),?\s+Ref ((?:\+|-)\d+),?\s+Will ((?:\+|-)\d+)/);
+		if(!save) return;
+		return {
+			start: start.index,
+			end:  index + hp.index + hp[0].length + save.index + save[0].length,
+			
+			ac: +start[1],
+			hdCount: hp[1] && hp[1].length? hp[1] : undefined,
+			health: hp[2],
+			healthNotes: hp[3] && hp[3].length? hp[3] : undefined,
+			fort: +save[1],
+			refl: +save[2],
+            will: +save[3]
+        };
+    }
+	
+	function nextLineWithPar(str, idx) {
+		let parCount = 0;
+		while(idx < str.length && (str.charAt(idx) !== '\n' || parCount)) {
+			if(str.charAt(idx) === '(') parCount++;
+			else if(str.charAt(idx) === ')') parCount--;
+			idx++;
+		}
+		while(idx < str.length && str.charAt(idx) === '\n') idx++;
+		return idx;
+	}
 }
 
 function cell(string) {
