0cbc8a849d5929db9f1450b718ce260d83a97532
Massimo Del Zotto <MassimoDZ8@gmail.com>
2016-05-05 08:10:43 +0200

    Initial testing of new BattleHelper.
    --HG--
    branch : adventuring

--- app/src/main/java/com/massimodz8/collaborativegrouporder/master/BattleHelper.java
+++ app/src/main/java/com/massimodz8/collaborativegrouporder/master/BattleHelper.java
@@ -2,9 +2,10 @@ package com.massimodz8.collaborativegrouporder.master;
 
 import android.support.annotation.NonNull;
 
+import com.massimodz8.collaborativegrouporder.ActorId;
 import com.massimodz8.collaborativegrouporder.InitiativeScore;
 
-import java.util.ArrayList;
+import java.util.ArrayDeque;
 import java.util.Arrays;
 import java.util.Comparator;
 
@@ -15,70 +16,92 @@ import java.util.Comparator;
 public class BattleHelper {
     public final InitiativeScore[] ordered;
 
+    public @ActorId int currentActor = -1;
     public int round = -1;
-    public int currentActor = -1;
+    public boolean fromReadiedStack;
     /**
      * Stack of triggered actions, they temporarily suppress normal order.
-     * Triggering a readied action is no real problem: it is a pre-spent action so it does have no permanent effects on order.
      * It also always happen while some other actor is acting. This is either null or contains at least 1 element.
-     * Do not mess with me. Go with IDs, they are truly persistent while objects might be not.
+     * Do not mess with me. Go with IDs, they are truly persistent while objects might be not, much less order.
      */
-    public ArrayList<Integer> triggered;
+    public ArrayDeque<Integer> triggered;
     public boolean orderChanged = true;
 
     public BattleHelper(@NonNull InitiativeScore[] ordered) {
         this.ordered = ordered;
     }
 
-    public void tickRound() {
-        if(round == -1) {
-            round = 1;
-            currentActor = 0;
-            return;
+    /**
+     * This was originally called 'tickRound' but what would that mean?
+     * It wants to figure out the next actor to (re)activate. Readied actions complicate the thing.
+     * @param really Sometimes you want to just know what will be next actor. If that's the case, set this to false.
+     *               This will prevent triggered actions to be popped which means you can restore previous state trivially by
+     *               setting this.currentActor to result of this function call.
+     *               Yes, it will also prevent incrementing the round count and other side-effects, turning this in a getCurrentActorID call.
+     * @return ID of previous actor (this.currentActor immediately before call)
+     */
+    public @ActorId int actorCompleted(boolean really) {
+        fromReadiedStack = false;
+        final int previd = shuffledPrevid < 0? currentActor : shuffledPrevid;
+        if(triggered != null) {
+            fromReadiedStack = really;
+            currentActor = really? triggered.pop() : triggered.getLast();
+            if(triggered.isEmpty()) triggered = null;
+            return previd;
         }
-        if(dontTick) { // then, this is nop.
-            dontTick = false;
-            return;
+        if(really) shuffledPrevid = -1;
+        int index = 0;
+        for (InitiativeScore check : ordered) {
+            if(check.actorID == currentActor) break;
+            index++;
         }
-        int next = currentActor + 1;
-        while(next < ordered.length && !ordered[next].enabled) next++;
+        int next = index + 1;
+        while (next < ordered.length && !ordered[next].enabled) next++;
         next %= ordered.length;
-        while(next < currentActor && !ordered[next].enabled) next++;
-        if(next <= currentActor) round++;
-        currentActor = next;
+        while (next < index && !ordered[next].enabled) next++;
+        if (next <= index && really) {
+            if(noRoundIncrement) {
+                round--;
+                noRoundIncrement = false;
+            }
+            round++;
+        }
+        currentActor = ordered[next].actorID;
+        return previd;
     }
 
     /**
-     * Shuffle the initiative order by moving currentActor to a new position and inferring its
-     * throw value so it stays coherent with 'global' order to be used when a new actor is added.
-     * @param newPos Current actor will move back this one and take its place, unless this is last.
+     * You want currentActor to act immediately before actor at the given position index.
+     * @param newPos Current actor will be placed here.
      */
-    public boolean shuffleCurrent(int newPos) {
-        if(currentActor == newPos) return false; // it happens with readied actions and I'm lazy
-        final InitiativeScore me = ordered[currentActor];
-        final InitiativeScore next = ordered[(currentActor + 1) % ordered.length];
-        // Setting the .rand so it sorts correctly is complicated. Easier to rebuild them in a
-        // predictable way so I can trivially infer a value transform.
-        int count = 0;
+    public boolean before(int newPos) {
+        int curSlot = 0;
         for (InitiativeScore el : ordered) {
-            el.rand = count;
-            count += 10;
+            if(el.actorID == currentActor) break;
+            curSlot++;
         }
-        me.initRoll = ordered[newPos].initRoll;
-        me.rand = ordered[newPos].rand;
-        me.rand += newPos != 0? 1 : -1;
-        Arrays.sort(ordered, new Comparator<InitiativeScore>() {
-            @Override
-            public int compare(InitiativeScore left, InitiativeScore right) {
-                if(left.rand < right.rand) return -1;
-                if(left.rand == right.rand) return 0;
-                return 1;
-            }
-        });
-        dontTick = next == ordered[currentActor];
+        if(curSlot == newPos) return false; // it happens with readied actions and I'm lazy
+        if(curSlot + 1 == newPos) return false;
+        shuffledPrevid = currentActor;
+        final InitiativeScore mover = ordered[curSlot];
+        final int step = newPos < curSlot? -1 : 1;
+        for(int index = curSlot; index != newPos; index += step) ordered[index] = ordered[index + step];
+        ordered[newPos] = mover;
+        if(newPos != 0 && newPos > curSlot) {
+            InitiativeScore temp = ordered[newPos];
+            ordered[newPos] = ordered[newPos - 1];
+            ordered[newPos - 1] = temp;
+        }
+        final int previ;
+        if(newPos != 0) previ = newPos - 1;
+        else {
+            previ = ordered.length - 1;
+            noRoundIncrement = true;
+        }
+        currentActor = ordered[previ].actorID;
         orderChanged = true;
         return true;
     }
-
-    private boolean dontTick;
+    boolean noRoundIncrement;
+    int shuffledPrevid = -1;
 }
