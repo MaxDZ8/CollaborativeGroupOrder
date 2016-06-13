2426580be690564bb4320eb3bba1fcf9f8a24214
Massimo Del Zotto <MassimoDZ8@gmail.com>
2016-06-13 18:36:26 +0200

    Look at this shit. .onActivityResult can be called on a 'suspended' Activity! FUCK
    --HG--
    branch : resistInstaActDestroy

--- app/src/main/java/com/massimodz8/collaborativegrouporder/SelectFormingGroupActivity.java
+++ app/src/main/java/com/massimodz8/collaborativegrouporder/SelectFormingGroupActivity.java
@@ -111,6 +111,36 @@ public class SelectFormingGroupActivity extends AppCompatActivity implements Acc
         //super.onSaveInstanceState(out);
     }
 
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
     // AccumulatingDiscoveryListener.OnTick vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
 
     @Override
